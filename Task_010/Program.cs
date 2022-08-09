71abf12adaac56ad46dc6fff166782a3a1971e28
Tatiana <tatum@rambler.ru>
2022-08-09 22:15:00 +0300

    Task 010

---
+++ Task_010/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100 && number < 1000)
+{
+    int digit = (number/10)%10;
+    Console.WriteLine($"Вторая цифра числа {number} равна {digit}");
+}
+else
+{
+    Console.WriteLine("Введенное число не трехзначное");
+}
