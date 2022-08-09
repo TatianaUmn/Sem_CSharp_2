e90dc26e51d045e93ad43b066d207bd765603a4f
Tatiana <tatum@rambler.ru>
2022-08-09 23:35:52 +0300

    Task 013

---
+++ Task013/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str_number = number.ToString();
+if(number > 99)
+{
+    Console.WriteLine("Третья цифра " + str_number[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
