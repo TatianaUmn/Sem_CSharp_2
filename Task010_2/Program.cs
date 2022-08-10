6219b5fef65b79e84bca37c0cbfe9f8d313e88e2
Tatiana <tatum@rambler.ru>
2022-08-10 15:44:59 +0300

    Task 010 variant 2

---
+++ Task010_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str_number = number.ToString();
+if(number > 99 && number < 1000)
+{
+    Console.WriteLine("Вторая цифра " + str_number[1]);
+}
+else
+{
+    Console.WriteLine("Вы ввели неправильное число");
+}
