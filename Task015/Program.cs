bcf9206cde0aedc306c3a56b6145bf423543fcae
Tatiana <tatum@rambler.ru>
2022-08-10 15:09:41 +0300

    Task 015

---
+++ Task015/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число от 1 до 7 включительно: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 6 || number == 7)
+{
+    Console.WriteLine("Да, день является выходным");
+}
+else if (number < 1 || number > 7)
+    {
+        Console.WriteLine("Вы ввели неправильное число");
+    }
+else
+{
+    Console.WriteLine("Нет, этот день не выходной");
+}
